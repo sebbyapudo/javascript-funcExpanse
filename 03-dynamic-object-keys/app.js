8edf1f5b220abc034a4308c855d79d4749a26ac1
sebbyapudo <sebbyapudo@gmail.com>
2023-07-17 18:24:27 +0300

    Created an object and dynamically appended property key value pairs on the fly

--- 03-dynamic-object-keys/app.js
+++ 03-dynamic-object-keys/app.js
@@ -1,2 +1,41 @@
 
 // Dynamic property keys 
+const person = {
+  name: "Sebby",
+};
+console.log(person['name']);
+person.age = 22;
+console.log(person);
+
+const items = {
+  'featured-items': ['item1', 'Item2', 'Item3'],
+};
+
+console.log(items["featured-items"]);
+
+let appState = 'loading';
+appState = 'error'
+const keyName = 'computer'
+
+const app = {
+  [appState]: true
+}
+
+app[keyName] = 'apple'
+console.log(app);
+
+const state = {
+  laoding:true,
+  name: '',
+  job: '',
+}
+
+function updateState(key,value){
+  state[key] = value;
+}
+
+updateState('name','Sebby')
+updateState('job','Developer')
+updateState('loading','false')
+updateState('products',[])
+console.log(state);
